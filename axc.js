be564c23137d99e717318b5cb5ca8ea3fb28e9ec
Nice <47160032+huhua0310@users.noreply.github.com>
2021-01-18 16:49:00 +0800

    Update axc.js

--- axc.js
+++ axc.js
@@ -31,8 +31,8 @@ const $ = new Env('工业品爱消除');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let inviteCodes = [
-  '879357@883981',
-  '883981@879357',
+  '879357@883981@883981',
+  '883981@879357@883981',
 ]
 let exchangeName = $.isNode() ? (process.env.EXCHANGE_GYEC ? process.env.EXCHANGE_GYEC : '1888京豆') : ($.getdata('JDGYEC') ? $.getdata('JDGYEC') : '1888京豆')
 //Node.js用户请在jdCookie.js处填写京东ck;
